5bfd6f1c1ba747155556a6b24ead8c44e1354c5b
hung61601 <hung61601@gmail.com>
2022-01-05 20:42:03 +0800

    add main funtion

--- src/grade/LetterGrade.java
+++ src/grade/LetterGrade.java
@@ -1,5 +1,9 @@
 package grade;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class LetterGrade {
 	public static char letterGrade(int score) {
 		char grade;
@@ -16,5 +20,19 @@ public class LetterGrade {
 		else
 			grade = 'F';
 		return grade;
+	}
+	public static void main(String[] args) {
+		System.out.print("Enetr the score = ");
+		try {
+			InputStreamReader isr = new InputStreamReader(System.in);
+			BufferedReader br = new BufferedReader(isr);
+			int score = Integer.parseInt(br.readLine());
+			char grade = letterGrade(score);
+			System.out.println("The grade of " + score + " is " + grade);
+		} catch (NumberFormatException ex) {
+			System.out.println("Not an integer!");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 }
